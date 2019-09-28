022828440aab3b9780de71e340ecc599c9a4bf39
artyemk <syscall0x80@gmail.com>
2019-09-28 18:13:10 +0500

    commit

--- front/src/index.js
+++ front/src/index.js
@@ -305,7 +305,7 @@ class VKchallenge extends React.Component {
 
             {this.state.activeTab5 === 'popular' ? <Group>
               {this.state.all_challenges.length > 0 &&
-                <List>
+                <List> keks
                   {/* {this.state.all_challenges.map((item) => (
                     
                   )
@@ -356,7 +356,7 @@ class VKchallenge extends React.Component {
                 <List>
                   {this.state.challenge_obj.map((item) => (
                     item.status !== "STOPPED" &&
-                    <Cell multiline onClick={() => { this.get_one_challenge(item._id); this.setState({ activeStory: 'challenge_info' }) }} before={<Avatar type="image" src="https://pp.userapi.com/c841025/v841025503/617f7/bkN1Def0s14.jpg" />}
+                    <Cell onClick={() => { this.get_one_challenge(item._id); this.setState({ activeStory: 'challenge_info' }) }} before={<Avatar type="image" src="https://pp.userapi.com/c841025/v841025503/617f7/bkN1Def0s14.jpg" />}
                       description={item.name} asideContent={< Icon24Play fill="var(--accent)" />}> {item.description}</Cell>
                   )
                   )}
@@ -368,7 +368,7 @@ class VKchallenge extends React.Component {
                 <List>
                   {this.state.challenge_obj.map((item) => (
                     item.status === "STOPPED" &&
-                    <Cell multiline before={<Avatar type="image" src="https://pp.userapi.com/c841025/v841025503/617f7/bkN1Def0s14.jpg" />} description={item.name} asideContent={<Icon24Add fill="var(--accent)" />}> {item.description}</Cell>
+                    <Cell before={<Avatar type="image" src="https://pp.userapi.com/c841025/v841025503/617f7/bkN1Def0s14.jpg" />} description={item.name} asideContent={<Icon24Add fill="var(--accent)" />}> {item.description}</Cell>
                   )
                   )}
                 </List>
