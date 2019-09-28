7b442bd659bd794029f60b565790cb11f4ced376
artyemk <syscall0x80@gmail.com>
2019-09-28 11:49:38 +0500

    commit

--- front/src/index.js
+++ front/src/index.js
@@ -262,6 +262,7 @@ class VKchallenge extends React.Component {
               {this.state.challenge_obj.length > 0 &&
                 <List>
                   {this.state.challenge_obj.map((item) => {
+                    alert(item.status)
                     if (item.status !== "STOPPED") {
                       <Cell before={<Avatar type="image" src="https://pp.userapi.com/c841025/v841025503/617f7/bkN1Def0s14.jpg" />} description={item.name} asideContent={<Icon24Add fill="var(--accent)" />}> {item.description}</Cell>
                     }
@@ -274,6 +275,7 @@ class VKchallenge extends React.Component {
               {this.state.challenge_obj.length > 0 &&
                 <List>
                   {this.state.challenge_obj.map((item) => {
+                    alert(item.status)
                     if (item.status === "STOPPED") {
                       <Cell before={<Avatar type="image" src="https://pp.userapi.com/c841025/v841025503/617f7/bkN1Def0s14.jpg" />} description={item.name} asideContent={<Icon24Add fill="var(--accent)" />}> {item.description}</Cell>
                     }
