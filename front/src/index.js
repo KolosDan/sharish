9ec55987580b058ce8001101e45891b6ec9a3ead
Artem <syscall0x80@gmail.com>
2019-09-28 07:46:38 +0500

    commit

--- front/src/index.js
+++ front/src/index.js
@@ -259,7 +259,7 @@ class VKchallenge extends React.Component {
             {this.state.challenge_obj.length > 0 &&
             <List>
             {this.state.challenge_obj.map((item) => (
-               <Cell before={<Avatar type="image" src="https://pp.userapi.com/c841025/v841025503/617f7/bkN1Def0s14.jpg" />} description="" asideContent={<Icon24MoreHorizontal fill="var(--accent)"/>}> asdas</Cell>
+               <Cell before={<Avatar type="image" src="https://pp.userapi.com/c841025/v841025503/617f7/bkN1Def0s14.jpg" />} description="" asideContent={<Icon24Add fill="var(--accent)"/>}> asdas</Cell>
             ))}
             </List>
             }
