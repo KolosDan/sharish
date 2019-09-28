4d120f64464fb8f1e7781abfdf86c009d8057f17
artyemk <syscall0x80@gmail.com>
2019-09-28 18:32:31 +0500

    commit

--- front/src/index.js
+++ front/src/index.js
@@ -310,6 +310,33 @@ class VKchallenge extends React.Component {
               </TabsItem>
             </Tabs>
 
+            <Card>
+      <CardActionArea>
+        <CardMedia
+          // className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+
             {this.state.activeTab5 === 'popular' ? <Group>
               {this.state.all_challenges.length > 0 &&
                 <List>
