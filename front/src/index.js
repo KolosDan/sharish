b205ca08d2e0747bbf322612d4d8dea0a484c94e
artyemk <syscall0x80@gmail.com>
2019-09-29 04:32:04 +0500

    commit

--- front/src/index.js
+++ front/src/index.js
@@ -351,17 +351,7 @@ class VKchallenge extends React.Component {
             <PanelHeader>
               Челлендж дня
               </PanelHeader>
-              <Group title="Big avatar (80px)">
-              <Cell
-                photo="https://pp.userapi.com/c841034/v841034569/3b8c1/pt3sOw_qhfg.jpg"
-                description="VKontakte"
-                bottomContent={<Button>Добавить</Button>}
-                before={<Avatar src="https://pp.userapi.com/c841034/v841034569/3b8c1/pt3sOw_qhfg.jpg" size={80}/>}
-                size="l"
-              >
-                Артур Стамбульцян
-              </Cell>
-              </Group>
+              <Avatar src="https://pp.userapi.com/c841034/v841034569/3b8c1/pt3sOw_qhfg.jpg" size={80}/>
           </Panel>
         </View>
 
