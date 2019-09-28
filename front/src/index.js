50d2ca7386eaafbfff73981d72c19056af479999
artyemk <syscall0x80@gmail.com>
2019-09-29 01:24:18 +0500

    commit

--- front/src/index.js
+++ front/src/index.js
@@ -287,11 +287,11 @@ class VKchallenge extends React.Component {
     this.setState({ name: args.name });
     this.setState({ desc: args.description});
     this.setState({ complete: args.complete_message });
-    this.setState({ task_list:  args.tasks});
-    this.setState({ max: args.max_participants });
-    this.setState({ hash: args.challenge_hashtag });
-    this.setState({ community: args.group_publisher});
-    this.setState({ winner: args.winner});
+    // this.setState({ task_list:  args.tasks});
+    // this.setState({ max: args.max_participants });
+    // this.setState({ hash: args.challenge_hashtag });
+    // this.setState({ community: args.group_publisher});
+    // this.setState({ winner: args.winner});
   }
 
   onChange(e) {
@@ -417,7 +417,7 @@ class VKchallenge extends React.Component {
                       <Cell
                         size="l"
                         description={"#" + item.hashtag}
-                        asideContent={<Icon28Write onClick={() => { this.setState({ activeStory: 'create' }) }} fill="var(--accent)" />}
+                        asideContent={<Icon28Write onClick={() => {this.restoreState(item); this.setState({ activeStory: 'create' }) }} fill="var(--accent)" />}
                         before={<Avatar src={item.user_photo} />}
                       >
                         {item.first_name} {item.last_name}
