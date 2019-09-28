941a671c8ab7abbbca5b3e2add1696352863fef1
artyemk <syscall0x80@gmail.com>
2019-09-29 03:54:33 +0500

    commit

--- front/src/index.js
+++ front/src/index.js
@@ -606,8 +606,8 @@ class VKchallenge extends React.Component {
 
         <View activePanel="ch_info" id="challenge_info">
           <Panel id="ch_info">
-            {alert(this.state.one_challenge_obj)}
-            <ChallengeInfo name={this.state.one_challenge_obj.name} desc={this.state.one_challenge_obj.description}
+          { alert(JSON.stringify(this.state.one_challenge_obj, null, 4)) } 
+          <ChallengeInfo name={this.state.one_challenge_obj.name} desc={this.state.one_challenge_obj.description}
               cover={this.state.one_challenge_obj.cover} tasks={this.state.one_challenge_obj.tasks} />
           </Panel>
         </View>
