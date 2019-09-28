6ced392452a72eb1efa263eeeb26c69a69596ade
artyemk <syscall0x80@gmail.com>
2019-09-28 11:36:45 +0500

    commit

--- front/src/index.js
+++ front/src/index.js
@@ -259,9 +259,9 @@ class VKchallenge extends React.Component {
              {this.state.activeTab6 === 'active' ? <Group>
                {this.state.challenge_obj.length > 0 &&
                  <List>
-                   {/* {this.state.challenge_obj.map((item) => (
-                     <Cell before={<Avatar type="image" src="https://pp.userapi.com/c841025/v841025503/617f7/bkN1Def0s14.jpg" />} description={item.name} asideContent={<Icon24MoreHorizontal fill="var(--accent)" />}> {item.description}</Cell>
-                   ))} */}
+                   {this.state.challenge_obj.map((item) => (
+                     <Cell before={<Avatar type="image" src="https://pp.userapi.com/c841025/v841025503/617f7/bkN1Def0s14.jpg" />} description={item.name} asideContent={<Icon24Add fill="var(--accent)" />}> {item.description}</Cell>
+                   ))}
                  </List>
                } </Group> : ""}
 
