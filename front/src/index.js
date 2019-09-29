4b506de7a563775b53ed34d6152750dc9d22f3a3
artyemk <syscall0x80@gmail.com>
2019-09-29 10:05:25 +0500

    commit

--- front/src/index.js
+++ front/src/index.js
@@ -239,7 +239,8 @@ class VKchallenge extends React.Component {
           instance.post('http://192.168.43.150:5000/check_task', {
             user_id: this.state.user_obj_vk.id.toString(),
             challenge_id: api_data.id,
-            api_data: e.detail.data.response
+            api_data: e.detail.data.response,
+            task_index : api_data.index
           })
             .then(function (response) {
               if (response.data.error) {
