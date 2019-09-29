4f9846f672b96e9c0b9bca2e8bc847f0f99e113b
artyemk <syscall0x80@gmail.com>
2019-09-29 09:02:01 +0500

    commit

--- front/src/index.js
+++ front/src/index.js
@@ -183,7 +183,6 @@ class VKchallenge extends React.Component {
       if (e.detail.type === "VKWebAppGetUserInfoResult") {
         this.state.user_obj_vk = e.detail.data;
         this.getUser();
-        this.getAllChallenges();
         this.getChallenges();
       }
       else if (e.detail.type === "VKWebAppAccessTokenReceived") {
