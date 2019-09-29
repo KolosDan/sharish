58cafa6fcc8e8907f20d5192ff350d516c94dc9d
artyemk <syscall0x80@gmail.com>
2019-09-29 12:36:24 +0500

    commit

--- front/src/index.js
+++ front/src/index.js
@@ -43,6 +43,7 @@ connect.send("VKWebAppInit", {});
 
 class ChallengeInfo extends React.Component {
   constructor(props) {
+    alert(JSON.stringify(this.props.challenge, null, 4)) 
     super(props);
 
     this.state = {
